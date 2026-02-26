c57548362ebe3ee31b9627dc081f387db7a335c9
github-action-benchmark <github@users.noreply.github.com>
2026-02-26 12:07:58 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for d8abeccd562adbd4d3e4ee131d659d661910f02c

---
+++ dev/bench/scan-perf/data.js
@@ -0,0 +1,55 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1772107678550,
+  "repoUrl": "https://github.com/mbutlerw/xtdb",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Matt Butler",
+            "username": "mbutlerw",
+            "email": "mattb-w@hotmail.co.uk"
+          },
+          "committer": {
+            "name": "Matt Butler",
+            "username": "mbutlerw",
+            "email": "mattb-w@hotmail.co.uk"
+          },
+          "id": "d8abeccd562adbd4d3e4ee131d659d661910f02c",
+          "message": "fixup: give workflow write perms",
+          "timestamp": "2026-02-26T11:59:38Z",
+          "url": "https://github.com/mbutlerw/xtdb/commit/d8abeccd562adbd4d3e4ee131d659d661910f02c"
+        },
+        "date": 1772107677710,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "profile-scan-points (n=100000,id=uuid)",
+            "value": 1497,
+            "unit": "ms"
+          },
+          {
+            "name": "profile-scan-sets (n=100000,id=uuid)",
+            "value": 13838,
+            "unit": "ms"
+          },
+          {
+            "name": "profile-scan-content-key (n=100000,id=uuid)",
+            "value": 5062,
+            "unit": "ms"
+          },
+          {
+            "name": "profile-scan-filters (n=100000,id=uuid)",
+            "value": 4004,
+            "unit": "ms"
+          },
+          {
+            "name": "summary (n=100000,id=uuid)",
+            "value": 35182,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
